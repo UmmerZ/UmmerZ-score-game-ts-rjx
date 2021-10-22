874fea6efd85dbbead8905775ebc72a67c2bab94
UmmerZ <69335478+UmmerZ@users.noreply.github.com>
2021-10-21 20:39:03 -0600

    added local storage state

--- src/users/List.tsx
+++ src/users/List.tsx
@@ -3,16 +3,19 @@ import { useStyles } from "./style"
 import React from "react"
 import { BehaviorSubject, map } from "rxjs"
 
-export const Board= () => {
-  const defaultUsers: IUser[] = [{
-    _id: "2584566",
-    username: "Loki",
-    score: 170,
-    }, {
-      _id:"58658",
+export const Board = () => {
+  const defaultUsers: IUser[] = [
+    {
+      _id: "2584566",
+      username: "Loki",
+      score: 170,
+    },
+    {
+      _id: "58658",
       username: "Thor",
-      score: 155
-    }]
+      score: 155,
+    },
+  ]
   let numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0]
   const [users, setUsers] = React.useState<IUser[]>(defaultUsers)
   const [currentUser, setCurrentUser] = React.useState<IUser | undefined>()
@@ -27,28 +30,16 @@ export const Board= () => {
 
   gameUsers$.next(users)
 
-
   React.useEffect(() => {
     userWithHighestScore$.subscribe(setWinner)
-  }, [])
+  }, [users])
+  const userWithHighestScore$ = gameUsers$.pipe(
+    map(user => user?.filter(x => x.score === highestScore()).map(x => x))
+  )
 
   const highestScore = () => {
-    const win  =  users.map(x => x.score )
-    return  Math.max(...win)
-  }
-
-  const userWithHighestScore$ = gameUsers$.pipe(
-    map(user => user?.filter(x =>  x.score === highestScore()).map(x => x)
-  ))
- 
-
-  const classes = useStyles()
-  const set = (arr: Array<number>, index: number, val: number, pos: number) => {
-    if (index < arr.length) {
-      return [...arr.slice(0, pos), val, ...arr.slice(pos + 1)]
-    } else {
-      return [...arr, ...Array(index - arr.length), val]
-    }
+    const win = users.map(x => x.score)
+    return Math.max(...win)
   }
   const countOccurrences = (arr: Array<number>, n: number, x: number) => {
     let res = 0
@@ -57,13 +48,48 @@ export const Board= () => {
     }
     return res
   }
+  const gameHasEnded = countOccurrences(value, 9, 0) < 7
+
+  React.useEffect(() => {
+    const data = localStorage.getItem("game-users")
+    if (data) {
+      setUsers(JSON.parse(data))
+    }
+  }, [])
+  React.useEffect(() => {
+    localStorage.setItem("game-users", JSON.stringify(users))
+  })
+  React.useEffect(() => {
+    if (currentUser) {
+      setUsers([
+        ...users,
+        {
+          _id: currentUser._id
+            ? currentUser._id
+            : Math.floor(Math.random() * 500000).toString(),
+          username: currentUser.username,
+          score: value.reduce((a: number, b: number) => a + b, 0),
+        },
+      ])
+    }
+  }, [gameHasEnded])
+  const transform = (x: string) =>
+    " " + x.charAt(0).toUpperCase() + x.slice(1).toLowerCase()
+  const classes = useStyles()
+  const set = (arr: Array<number>, index: number, val: number, pos: number) => {
+    if (index < arr.length) {
+      return [...arr.slice(0, pos), val, ...arr.slice(pos + 1)]
+    } else {
+      return [...arr, ...Array(index - arr.length), val]
+    }
+  }
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setCurrentUser(newUser)
   }
   const prevScore = winner?.map(x => x.score)
-  const gameHasEnded = countOccurrences(value, 9, 0) < 7
-  console.log(winner)
+
   return (
     <div className={classes.root}>
       <div className={classes.total}>
@@ -72,9 +98,9 @@ export const Board= () => {
           : `You get 3 clicks to score`}
       </div>
       {currentUser && "_id" in currentUser ? (
-        <div
-          className={classes.welcome}
-        >{`Hi ${currentUser.username}! Lets Play`}</div>
+        <div className={classes.welcome}>{`Hi ${transform(
+          currentUser.username
+        )}! Lets Play`}</div>
       ) : (
         <div className={classes.input}>
           <form onSubmit={handleSubmit}>
@@ -84,9 +110,9 @@ export const Board= () => {
               variant="outlined"
               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                 setNewUser({
-                  _id: Math.floor(Math.random() * 150 + 10).toString(),
+                  _id: Math.floor(Math.random() * 500000).toString(),
                   username: e.target.value,
-                  score: 0
+                  score: 0,
                 })
               }
             />
@@ -123,15 +149,14 @@ export const Board= () => {
           {clicked ? value[index] : button}
         </Button>
       ))}
-      {gameHasEnded ?
-       
-       
-       <div className= {classes.welcome}>
-            {prevScore && value.reduce((a: number, b: number) => a + b, 0) >   prevScore[0] ?
-          `You are the Winner` : 
-          `Oops! Winner is ${winner?.map(x => x.username)}`}</div>
-          : null }
-    </div> 
- 
+      {gameHasEnded ? (
+        <div className={classes.welcome}>
+          {prevScore &&
+          value.reduce((a: number, b: number) => a + b, 0) > prevScore[0]
+            ? `You are the Winner`
+            : `Oops! Winner is ${winner?.map(x => x.username)}`}
+        </div>
+      ) : null}
+    </div>
   )
 }
